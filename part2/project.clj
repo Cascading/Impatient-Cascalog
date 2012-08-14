20c9067523ee2c95aed4f3cb663281c7c826d0dc
Paul Lam <paul.lam@forward.co.uk>
2012-08-14 18:26:18 +0100

    part 2 word count

---
+++ part2/project.clj
@@ -0,0 +1,11 @@
+(defproject impatient "0.1.0-SNAPSHOT"
+  :description "Cascalog for the Impatient - Part 2"
+  :url "https://github.com/Quantisan/Impatient/tree/cascalog/part2"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :uberjar-name "impatient.jar"
+  :aot [impatient.core]
+  :main impatient.core
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [cascalog "1.10.0"]
+                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]])
---
+++ part2/src/impatient/core.clj
@@ -0,0 +1,16 @@
+(ns impatient.core
+  (:use cascalog.api)
+  (:require [clojure.string :as s]
+            [cascalog.ops :as c])
+  (:gen-class))
+
+(defmapcatop split [line]
+  "reads in a line of string and splits it by regex"
+  (s/split #"\s+" line))
+
+(defn -main [in out & args]
+  (?<- (hfs-textline out)
+       [?word ?count]
+       ((hfs-textline in) ?line)
+       (split ?line :> ?word)
+       (c/count ?count)))
